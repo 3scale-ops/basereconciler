5d01597f59475b3be2efb13730300c70e13cd504
Roi Vazquez <roivazquez@gmail.com>
2023-05-18 10:23:44 +0200

    Implement status reconciliation for Deployment and StatefulSet based custom resources

---
+++ reconciler/status.go
@@ -0,0 +1,66 @@
+package reconciler
+
+import (
+	"context"
+
+	"github.com/3scale-ops/basereconciler/status"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func (r *Reconciler) ReconcileStatus(ctx context.Context, instance status.ObjectWithAppStatus,
+	deployments, statefulsets []types.NamespacedName, mutators ...func() bool) error {
+	logger := log.FromContext(ctx)
+	update := false
+	status := instance.GetStatus()
+
+	// Aggregate the status of all Deployments owned
+	// by this instance
+	for _, key := range deployments {
+		deployment := &appsv1.Deployment{}
+		deploymentStatus := status.GetDeploymentStatus(key)
+		if err := r.Client.Get(ctx, key, deployment); err != nil {
+			return err
+		}
+
+		if !equality.Semantic.DeepEqual(deploymentStatus, deployment.Status) {
+			status.SetDeploymentStatus(key, &deployment.Status)
+			update = true
+		}
+	}
+
+	// Aggregate the status of all StatefulSets owned
+	// by this instance
+	for _, key := range statefulsets {
+		sts := &appsv1.StatefulSet{}
+		stsStatus := status.GetStatefulSetStatus(key)
+		if err := r.Client.Get(ctx, key, sts); err != nil {
+			return err
+		}
+
+		if !equality.Semantic.DeepEqual(stsStatus, sts.Status) {
+			status.SetStatefulSetStatus(key, &sts.Status)
+			update = true
+		}
+	}
+
+	// TODO: calculate health
+
+	// call mutators
+	for _, fn := range mutators {
+		if fn() {
+			update = true
+		}
+	}
+
+	if update {
+		if err := r.Client.Status().Update(ctx, instance); err != nil {
+			logger.Error(err, "unable to update status")
+			return err
+		}
+	}
+
+	return nil
+}
